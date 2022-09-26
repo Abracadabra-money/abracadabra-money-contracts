08c7c7f8ca201ad1ac49885f039c480cda977966
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2022-09-25 21:49:38 -0400

    Solidly stable oracle (#3)
    * new spookswap wFTM/MIM strategy
    
    * test solidly stable oracle
    
    * simple oracle test
    
    * templates and get all velodrome stable pairs
    
    * solidly stable pair test deviation tests
    
    * test with some high and low liquidity pairs
    
    * pair skewing tests
    
    * removed import
    
    * output test result to csv for analytics
    
    * store relative file difference instead
    
    * fixed templates and archive tests
    
    * converted Scripts abstract contracts to Libraries
    
    * changed starting block
    
    * test with adding ons of liquidity and see how it affecs the price feed
    
    * Add rests with mock prices
    
    * Add doc explaining Solidly fair value calculation
    
    * (Fix some typos/formatting)
    
    Co-authored-by: Herman Bosma <dev@hermanbosma.com>

--- src/interfaces/IVelodromePairFactory.sol
+++ src/interfaces/IVelodromePairFactory.sol
@@ -1,7 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+import "interfaces/ISolidlyPair.sol";
+
 interface IVelodromePairFactory {
+    function allPairs(uint256 index) external view returns (ISolidlyPair);
+
+    function allPairsLength() external view returns (uint256);
+
     function volatileFee() external view returns (uint256);
+
     function stableFee() external view returns (uint256);
+
+    function createPair(address tokenA, address tokenB, bool stable) external returns (address pair);
 }
