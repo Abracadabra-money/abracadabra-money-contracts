f0ebab75c15ea64f2c67516f18842942c4b708c1
0xCalibur <0xCalibur@protonmail.com>
2022-09-25 21:42:34 -0400

    cleaner

--- utils/Constants.sol
+++ utils/Constants.sol
@@ -4,13 +4,13 @@ pragma solidity >=0.8.0;
 import "forge-std/Vm.sol";
 
 library ChainId {
-    uint256 public constant Mainnet = 1;
-    uint256 public constant BSC = 56;
-    uint256 public constant Polygon = 137;
-    uint256 public constant Fantom = 250;
-    uint256 public constant Optimism = 10;
-    uint256 public constant Arbitrum = 42161;
-    uint256 public constant Avalanche = 43114;
+    uint256 internal constant Mainnet = 1;
+    uint256 internal constant BSC = 56;
+    uint256 internal constant Polygon = 137;
+    uint256 internal constant Fantom = 250;
+    uint256 internal constant Optimism = 10;
+    uint256 internal constant Arbitrum = 42161;
+    uint256 internal constant Avalanche = 43114;
 }
 
 contract Constants {
