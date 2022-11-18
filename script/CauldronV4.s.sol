7aa8f7108349f472ed607eebb21216cf9a8589f2
0xCalibur <0xCalibur@protonmail.com>
2022-11-18 11:38:25 -0500

    wip

--- script/CauldronV4.s.sol
+++ script/CauldronV4.s.sol
@@ -4,14 +4,28 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import "utils/BaseScript.sol";
 import "cauldrons/CauldronV4.sol";
+import "periphery/DegenBoxOwner.sol";
 
 contract CauldronV4Script is BaseScript {
-    function run() public returns (CauldronV4 masterContract) {
+    function run() public returns (CauldronV4 masterContract, DegenBoxOwner degenBoxOwner) {
         vm.startBroadcast();
-    
-        masterContract = new CauldronV4(IBentoBoxV1(constants.getAddress("mainnet.degenBox")), IERC20(constants.getAddress("mainnet.mim")));
 
-        // Only when deploying live
+        if (block.chainid == ChainId.Mainnet) {
+            masterContract = new CauldronV4(
+                IBentoBoxV1(constants.getAddress("mainnet.degenBox")),
+                IERC20(constants.getAddress("mainnet.mim"))
+            );
+        }
+
+        if (block.chainid == ChainId.Arbitrum) {
+            IBentoBoxV1 degenBox = IBentoBoxV1(constants.getAddress("arbitrum.degenBox"));
+            masterContract = new CauldronV4(degenBox, IERC20(constants.getAddress("arbitrum.mim")));
+
+            degenBoxOwner = new DegenBoxOwner();
+            masterContract.transferOwnership(address(degenBoxOwner), true, false);
+            degenBoxOwner.setDegenBox(degenBox);
+        }
+
         if (!testing) {}
 
         vm.stopBroadcast();
