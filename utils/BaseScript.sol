5e945b3f87e6df4d35258d1f9d04ab9e1a81e287
0xCalibur <0xCalibur@protonmail.com>
2022-11-18 10:51:54 -0500

    Constants: new constants and chain info

--- utils/BaseScript.sol
+++ utils/BaseScript.sol
@@ -15,4 +15,8 @@ abstract contract BaseScript is Script {
     function setTesting(bool _testing) public {
         testing = _testing;
     }
+
+    function getChainIdKey() public view returns (uint256) {
+        return constants.getChainIdKey(block.chainid);
+    }
 }
