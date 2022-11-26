84b6acb5adb37d8700770abf6e967ed2bbd7b6bb
0xCalibur <0xCalibur@protonmail.com>
2022-11-26 10:59:17 -0500

    useless import

--- src/periphery/GmxGlpRewardHandler.sol
+++ src/periphery/GmxGlpRewardHandler.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-import "OpenZeppelin/utils/Address.sol";
 import "BoringSolidity/libraries/BoringERC20.sol";
 import "BoringSolidity/BoringOwnable.sol";
 import {GmxGlpWrapperData} from "tokens/GmxGlpWrapper.sol";
