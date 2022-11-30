065f243c19547b446eb288be7d442aefe611cd6a
0xCalibur <0xCalibur@protonmail.com>
2022-11-30 13:08:33 -0500

    nice to have, missing function in interface

--- src/interfaces/IGmxGlpRewardHandler.sol
+++ src/interfaces/IGmxGlpRewardHandler.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.8.0;
 
 import "BoringSolidity/interfaces/IERC20.sol";
+import "interfaces/IGmxRewardRouterV2.sol";
 
 interface IGmxGlpRewardHandler {
     function harvest() external;
@@ -13,4 +14,37 @@ interface IGmxGlpRewardHandler {
         address recipient,
         bytes calldata data
     ) external returns (uint256 amountOut);
+
+    function setFeeParameters(address _feeCollector, uint8 _feePercent) external;
+
+    function setRewardTokenEnabled(IERC20 token, bool enabled) external;
+
+    function setSwappingTokenOutEnabled(IERC20 token, bool enabled) external;
+
+    function setAllowedSwappingRecipient(address recipient, bool enabled) external;
+
+    function setRewardRouter(IGmxRewardRouterV2 _rewardRouter) external;
+
+    function setSwapper(address _swapper) external;
+
+    function unstakeGmx(uint256 amount, uint256 amountTransferToFeeCollector) external;
+
+    function unstakeEsGmxAndVest(
+        uint256 amount,
+        uint256 glpVesterDepositAmount,
+        uint256 gmxVesterDepositAmount
+    ) external;
+
+    function withdrawFromVesting(
+        bool withdrawFromGlpVester,
+        bool withdrawFromGmxVester,
+        bool stake
+    ) external;
+
+    function claimVestedGmx(
+        bool withdrawFromGlpVester,
+        bool withdrawFromGmxVester,
+        bool stake,
+        bool transferToFeeCollecter
+    ) external;
 }
