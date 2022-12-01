7808d913bb81e757c3d01f857a4909c33da3bb80
0xCalibur <0xCalibur@protonmail.com>
2022-12-01 11:59:43 -0500

    GLP Harvestor V2 with mutable parameters

--- utils/VelodromeLib.sol
+++ utils/VelodromeLib.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "harvesters/VelodromeVolatileLpHarvester.sol";
+import "periphery/VelodromeVolatileLpHarvester.sol";
 import "tokens/SolidlyLpWrapper.sol";
 
 library VelodromeLib {
