a9ec632cb23592deb8bf3ca7468a13dcb5f12fd9
0xCalibur <0xCalibur@protonmail.com>
2022-12-03 22:03:09 -0500

    First draft of mim cauldron distribution v3

--- src/periphery/MimCauldronDistributor.sol
+++ src/periphery/MimCauldronDistributor.sol
@@ -9,10 +9,23 @@ import "interfaces/IMimCauldronDistributor.sol";
 
 contract MimCauldronDistributor is BoringOwnable, IMimCauldronDistributor {
     event LogPaused(bool previous, bool current);
+    event LogCauldronParameterChanged(ICauldronV4 indexed cauldron, uint256 targetApy);
+
     error ErrPaused();
+    error ErrInvalidTargetApy(uint16);
+
+    struct CauldronInfo {
+        ICauldronV4 cauldron;
+        uint16 targetApyInBips;
+        uint256 apyCollateralShareValueCache;
+        IOracle oracle;
+        bytes oracleData;
+    }
+
+    uint256 public constant BIPS = 10_000;
 
     ERC20 public immutable mim;
-    ICauldronV4 public immutable cauldron;
+    CauldronInfo[] public cauldronInfos;
     bool public paused;
 
     modifier notPaused() {
@@ -22,25 +35,100 @@ contract MimCauldronDistributor is BoringOwnable, IMimCauldronDistributor {
         _;
     }
 
-    constructor(ERC20 _mim, ICauldronV4 _cauldron) {
+    constructor(ERC20 _mim) {
         mim = _mim;
-        cauldron = _cauldron;
     }
 
-    function distribute() external notPaused {
-        uint256 amount = mim.balanceOf(address(this));
-
-        Rebase memory totalBorrow = cauldron.totalBorrow();
-        if (amount > totalBorrow.elastic) {
-            amount = totalBorrow.elastic;
+    function setCauldronParameters(ICauldronV4 _cauldron, uint16 _targetApyInBips) external onlyOwner {
+        if (_targetApyInBips > BIPS) {
+            revert ErrInvalidTargetApy(_targetApyInBips);
         }
 
-        mim.transfer(address(cauldron), amount);
+        int256 index = _findCauldronInfo(_cauldron);
 
-        cauldron.repayForAll(
-            0, /* amount ignored when skimming */
-            true
-        );
+        if (index >= 0) {
+            if (_targetApyInBips > 0) {
+                cauldronInfos[uint256(index)].targetApyInBips = _targetApyInBips;
+            } else {
+                cauldronInfos[uint256(index)] = cauldronInfos[cauldronInfos.length - 1];
+                cauldronInfos.pop();
+            }
+        } else {
+            cauldronInfos.push(
+                CauldronInfo({
+                    cauldron: _cauldron,
+                    targetApyInBips: _targetApyInBips,
+                    oracle: _cauldron.oracle(),
+                    oracleData: _cauldron.oracleData(),
+                    apyCollateralShareValueCache: 0
+                })
+            );
+        }
+
+        emit LogCauldronParameterChanged(_cauldron, _targetApyInBips);
+    }
+
+    function getCauldronInfo(ICauldronV4 _cauldron) external view returns (CauldronInfo memory) {
+        return cauldronInfos[uint256(_findCauldronInfo(_cauldron))];
+    }
+
+    function _findCauldronInfo(ICauldronV4 _cauldron) private view returns (int256) {
+        for (uint256 i = 0; i < cauldronInfos.length; i++) {
+            if (cauldronInfos[i].cauldron == _cauldron) {
+                return int256(i);
+            }
+        }
+
+        return -1;
+    }
+
+    // take % apy on the collateral share and compute USD value with oracle
+    // then take this amount and how much that is on the sum of all cauldron'S apy USD
+
+    function distribute() public {
+        uint256 amount = mim.balanceOf(address(this));
+        uint256 allCauldronsTotalCollateralShareValue = 0;
+
+        // Gather all stats needed for the subsequent distribution
+        for (uint256 i = 0; i < cauldronInfos.length; i++) {
+            CauldronInfo storage info = cauldronInfos[i];
+
+            // compute the cauldron's total collateral share value in usd
+            //
+            // TODO: could we have the average oracle price from last distribution to now? Otherwise a cauldron
+            // might get more or less depending on the current market condition at distribution time.
+            uint256 totalCollateralShareValue = (info.cauldron.totalCollateralShare() * 1e18) / info.oracle.peekSpot(info.oracleData);
+
+            // how many USD represents the target apy
+            info.apyCollateralShareValueCache = (info.targetApyInBips * totalCollateralShareValue) / BIPS;
+
+            // sum all target usd apy
+            allCauldronsTotalCollateralShareValue += info.apyCollateralShareValueCache;
+        }
+
+        for (uint256 i = 0; i < cauldronInfos.length; i++) {
+            CauldronInfo storage info = cauldronInfos[i];
+
+            // what's the amount to distribute
+            uint256 distributionAmount = (info.apyCollateralShareValueCache * 1e18) / allCauldronsTotalCollateralShareValue;
+
+            if (distributionAmount > amount) {
+                distributionAmount = amount;
+            }
+
+            if (distributionAmount > 0) {
+                Rebase memory totalBorrow = info.cauldron.totalBorrow();
+                if (distributionAmount > totalBorrow.elastic) {
+                    distributionAmount = totalBorrow.elastic;
+                }
+
+                mim.transfer(address(info.cauldron), distributionAmount);
+
+                info.cauldron.repayForAll(0, true);
+
+                amount -= distributionAmount;
+            }
+        }
     }
 
     function setPaused(bool _paused) external onlyOwner {
