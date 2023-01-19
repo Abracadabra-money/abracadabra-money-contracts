e68d5d0e34865eb13c54e6b29742adcdaa5ba367
0xCalibur <0xCalibur@protonmail.com>
2023-01-18 19:12:25 -0500

    formatting & cleanup

--- src/periphery/DegenBoxHelper.sol
+++ src/periphery/DegenBoxHelper.sol
@@ -4,26 +4,36 @@ pragma solidity >=0.8.0;
 import "BoringSolidity/ERC20.sol";
 import "BoringSolidity/libraries/BoringRebase.sol";
 import "periphery/Operatable.sol";
-import "interfaces/ICauldronV4.sol";
+import "interfaces/ICauldronV2.sol";
 import "interfaces/IBentoBoxV1.sol";
 
 contract DegenBoxHelper is Operatable {
     using RebaseLibrary for Rebase;
 
-    IBentoBoxV1 immutable public degenBox;
-    IERC20 immutable public magicInternetMoney;
+    IBentoBoxV1 public immutable degenBox;
+    IERC20 public immutable magicInternetMoney;
 
-    constructor (IBentoBoxV1 degenBox_, IERC20 magicInternetMoney_) {
+    constructor(IBentoBoxV1 degenBox_, IERC20 magicInternetMoney_) {
         degenBox = degenBox_;
         magicInternetMoney = magicInternetMoney_;
         degenBox_.registerProtocol();
     }
 
-    function degenBoxDeposit(IERC20 token, address to, uint256 amount, uint256 share ) external payable onlyOperators returns (uint256, uint256) {
-        return degenBox.deposit{value: msg.value}(token, tx.origin, to, uint256(amount), uint256(share));
+    function degenBoxDeposit(
+        IERC20 token,
+        address to,
+        uint256 amount,
+        uint256 share
+    ) external payable onlyOperators returns (uint256, uint256) {
+        return degenBox.deposit{value: msg.value}(token, tx.origin, to, amount, share);
     }
 
-    function degenBoxWithdraw(IERC20 token, address to, uint256 amount, uint256 share) external onlyOperators returns (uint256, uint256) {
+    function degenBoxWithdraw(
+        IERC20 token,
+        address to,
+        uint256 amount,
+        uint256 share
+    ) external onlyOperators returns (uint256, uint256) {
         return degenBox.withdraw(token, tx.origin, to, amount, share);
     }
 
@@ -34,7 +44,7 @@ contract DegenBoxHelper is Operatable {
     /// @return amount The total amount repayed.
     function repayPart(
         address to,
-        ICauldronV4 cauldron,
+        ICauldronV2 cauldron,
         uint256 part
     ) public onlyOperators returns (uint256 amount) {
         Rebase memory totalBorrow = cauldron.totalBorrow();
@@ -44,5 +54,4 @@ contract DegenBoxHelper is Operatable {
         degenBox.transfer(magicInternetMoney, tx.origin, address(cauldron), share);
         cauldron.repay(to, true, part);
     }
-
 }
