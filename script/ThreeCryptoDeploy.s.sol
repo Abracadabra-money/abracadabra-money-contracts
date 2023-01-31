650697948114b105771ec64739e0a939b17685a4
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2023-01-30 20:17:18 -0500

    silent and private parse-deployment, trying out sh as default shell on mac

--- script/ThreeCryptoDeploy.s.sol
+++ script/ThreeCryptoDeploy.s.sol
@@ -18,35 +18,8 @@ contract ThreeCryptoDeployScript is BaseScript {
             ICauldronV4 cauldron
         )
     {
-        IBentoBoxV1 degenBox = IBentoBoxV1(constants.getAddress("mainnet.degenBox"));
-        address safe = constants.getAddress("mainnet.safe.ops");
-        address masterContract = constants.getAddress("mainnet.cauldronV4");
-
         vm.startBroadcast();
 
-        ThreeCryptoOracle threecryptooracle = new ThreeCryptoOracle(constants.getAddress("mainnet.y3Crypto"));
-
-        oracle = OracleLib.deploySimpleProxyOracle(threecryptooracle);
-
-        cauldron = CauldronLib.deployCauldronV4(
-            degenBox,
-            masterContract,
-            IERC20(constants.getAddress("mainnet.y3Crypto")),
-            oracle,
-            "",
-            9000, // 90% ltv
-            700, // 7% interests
-            0, // 0% opening
-            400 // 4% liquidation
-        );
-
-        levSwapper = new ThreeCryptoLevSwapper(degenBox);
-        swapper = new ThreeCryptoSwapper(degenBox);
-
-        // Only when deploying live
-        if (!testing) {
-            oracle.transferOwnership(safe, true, false);
-        }
 
         vm.stopBroadcast();
     }
