60a93f21b2b935db4482a5bd718d21c0da974b06
0xCalibur <0xCalibur@protonmail.com>
2023-06-01 20:54:44 -0400

    new deployments

--- tasks/core/forgeDeploy.js
+++ tasks/core/forgeDeploy.js
@@ -45,6 +45,8 @@ module.exports = async function (taskArgs, hre) {
                 process.exit(0);
             }
         }
+
+        await shell.exec(`rm -rf ${foundry.broadcast}`, { silent: true });
     }
 
     if (taskArgs.verify) {
@@ -58,7 +60,6 @@ module.exports = async function (taskArgs, hre) {
         process.exit(1);
     }
 
-    await shell.exec(`rm -rf ${foundry.broadcast}`, { silent: true });
     const cmd = `${env_args} forge script ${script} --rpc-url ${hre.network.config.url} ${broadcast_args} ${verify_args} ${resume_args} -vvvv --private-key *******`.replace(/\s+/g, ' ');
     console.log(cmd);
 
