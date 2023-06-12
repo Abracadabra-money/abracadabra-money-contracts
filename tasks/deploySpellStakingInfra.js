dfcc2ad34032c948435608de971b810c1e56b3ca
0xCalibur <0xCalibur@protonmail.com>
2023-06-12 19:31:16 -0400

    deploy mainnet first then the altchain

--- tasks/deploySpellStakingInfra.js
+++ tasks/deploySpellStakingInfra.js
@@ -2,15 +2,16 @@ const shell = require('shelljs');
 
 module.exports = async function (taskArgs, hre) {
     const { foundryDeployments } = hre;
+
+    await shell.exec("yarn build");
+    await hre.run("forge-deploy-multichain", { script: "SpellStakingRewardInfra", broadcast: taskArgs.broadcast, verify: taskArgs.verify, networks: ["mainnet"], noConfirm: taskArgs.noConfirm, resume: taskArgs.resume });
+
+    const networks = ["avalanche", /*"fantom", "arbitrum"*/];
     let mainnetDistributor = hre.ethers.constants.AddressZero;
 
     if (foundryDeployments.deploymentExists("Mainnet_SpellStakingRewardDistributor", 1)) {
         mainnetDistributor = (await foundryDeployments.getDeployment("Mainnet_SpellStakingRewardDistributor", 1)).address;
     }
-
-    const networks = ["mainnet", "avalanche", /*"fantom", "arbitrum"*/];
-
-    await shell.exec("yarn build");
 
     shell.env["MAINNET_DISTRIBUTOR"] = mainnetDistributor;
     await hre.run("forge-deploy-multichain", { script: "SpellStakingRewardInfra", broadcast: taskArgs.broadcast, verify: taskArgs.verify, networks, noConfirm: taskArgs.noConfirm, resume: taskArgs.resume });
