5868e6f97fec7894e442e849c197584f410aa59a
0xCalibur <0xCalibur@protonmail.com>
2023-07-04 00:48:34 -0400

    wip

--- init.js
+++ init.js
@@ -10,8 +10,8 @@ const destination = process.argv[2];
     await Promise.all(Object.keys(libs).map(async (target) => {
         const { url, commit } = libs[target];
         console.log(`✨Installing ${target} from ${url} at ${commit}`);
-        await shell.exec(`git clone --recurse-submodules ${url} ${destination}/${target}`, { silent: true, fatal: true });
-        await shell.exec(`git checkout ${commit}`, { silent: true, fatal: true });
+        await shell.exec(`git clone --recurse-submodules ${url} ${destination}/${target}`, { silent: true, fatal: true, });
+        await shell.exec(`git checkout ${commit}`, { silent: true, fatal: true, cwd: `${destination}/${target}` });
         await rimraf(`${destination}/${target}/**/.git`);
     }));
 })();
