f2c484de8bf031a8950b4af10b11936affdab4b2
0xCalibur <0xCalibur@protonmail.com>
2023-07-05 10:23:47 -0400

    ChainOps: avoid reinstalling libs when already cloned at the right commit hash

--- init.js
+++ init.js
@@ -1,19 +1,37 @@
 const { libs } = require('./package.json');
 const { rimraf } = require('rimraf')
 const shell = require('shelljs');
+const { access, open, readdir, constants } = require('node:fs/promises');
 
 const destination = process.argv[2];
 
 (async () => {
-    await rimraf(destination);
+    // delete all folder not in libs
+    await Promise.all((await readdir(destination)).map(async (folder) => {
+        if (!libs[folder]) {
+            await rimraf(`${destination}/${folder}`);
+        }
+    }));
 
     await Promise.all(Object.keys(libs).map(async (target) => {
         const { url, commit } = libs[target];
-        console.log(`✨Installing ${target} from ${url} at ${commit}`);
-        await shell.exec(`git clone --recurse-submodules ${url} ${destination}/${target}`, { silent: true, fatal: true, });
-        await shell.exec(`git checkout ${commit}`, { silent: true, fatal: true, cwd: `${destination}/${target}` });
-        await rimraf(`${destination}/${target}/**/.git`);
+
+        const dotHashfile = `${destination}/${target}/.${commit}`;
+        try {
+            if (await access(dotHashfile), constants.R_OK) {
+                console.log(`✨${target} already installed`);
+                return;
+            }
+        } catch { }
+
+        const dest = `${destination}/${target}`;
+        await rimraf(dest);
+
+        console.log(`✨ Installing ${url}#${commit} to ${target}`);
+        await shell.exec(`git clone --recurse-submodules ${url} ${dest}`, { silent: true, fatal: true, });
+        await shell.exec(`git checkout ${commit}`, { silent: true, fatal: true, cwd: `${dest}` });
+        await rimraf(`${dest}/**/.git`);
+
+        await (await open(dotHashfile, 'a')).close();
     }));
 })();
-
-
