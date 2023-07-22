42f39a6aa2063a32b99ae243cc4fd070488c0bc4
0xCalibur <0xCalibur@protonmail.com>
2023-07-22 12:57:04 -0400

    Moonriver Deployments: - DegenBox - CauldronV4 MasterContract - CauldronOwner

--- script/CauldronV4.s.sol
+++ script/CauldronV4.s.sol
@@ -13,17 +13,25 @@ contract CauldronV4Script is BaseScript {
         address safe = toolkit.getAddress(block.chainid, "safe.ops");
         ERC20 mim = ERC20(toolkit.getAddress(block.chainid, "mim"));
 
-        CauldronOwner owner = deployer.deploy_CauldronOwner(toolkit.prefixWithChainName(block.chainid, "CauldronOwner"), safe, mim);
+        CauldronOwner cauldronOwner = deployer.deploy_CauldronOwner(toolkit.prefixWithChainName(block.chainid, "CauldronOwner"), safe, mim);
         CauldronV4 cauldronV4MC = deployer.deploy_CauldronV4(toolkit.prefixWithChainName(block.chainid, "CauldronV4"), degenBox, mim);
 
         vm.startBroadcast();
         if (!testing()) {
-            owner.setOperator(safe, true);
-            owner.transferOwnership(safe, true, false);
-            cauldronV4MC.setFeeTo(safe);
-            cauldronV4MC.transferOwnership(address(safe), true, false);
-        }
+            if (cauldronOwner.owner() == tx.origin) {
+                if (!cauldronOwner.operators(safe)) {
+                    cauldronOwner.setOperator(safe, true);
+                }
+                cauldronOwner.transferOwnership(safe, true, false);
+            }
 
+            if (cauldronV4MC.owner() == tx.origin) {
+                if (cauldronV4MC.feeTo() != safe) {
+                    cauldronV4MC.setFeeTo(safe);
+                }
+                cauldronV4MC.transferOwnership(address(safe), true, false);
+            }
+        }
         vm.stopBroadcast();
     }
 }
