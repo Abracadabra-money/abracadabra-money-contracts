a8ce217eb7b799c7f29c7ea2474f2ec6bae43cb6
0xCalibur <0xCalibur@protonmail.com>
2023-07-30 20:05:03 -0400

    wip script for magic curvelp mim/usdt cauldron - degenbox wrapper - swappers - cauldron proxy

--- script/MagicCurveLp.s.sol
+++ script/MagicCurveLp.s.sol
@@ -2,12 +2,18 @@
 pragma solidity >=0.8.0;
 
 import "utils/BaseScript.sol";
+import "utils/CauldronDeployLib.sol";
 import "oracles/ProxyOracle.sol";
 import "tokens/MagicCurveLp.sol";
 import "periphery/MagicCurveLpHarvestor.sol";
 import "periphery/MagicCurveLpRewardHandler.sol";
 import "tokens/MagicCurveLp.sol";
+import "interfaces/ISwapperV2.sol";
+import "interfaces/ILevSwapperV2.sol";
 import "interfaces/ICurveRewardGauge.sol";
+import "interfaces/ICauldronV4.sol";
+import "interfaces/IBentoBoxV1.sol";
+import "interfaces/ICurvePool.sol";
 import "interfaces/IERC4626.sol";
 
 contract MagicCurveLpScript is BaseScript {
@@ -15,14 +21,15 @@ contract MagicCurveLpScript is BaseScript {
 
     function deploy() public returns (MagicCurveLp vault, MagicCurveLpHarvestor harvestor) {
         if (block.chainid == ChainId.Kava) {
-            return _deployKavaMimUsdtVault();
+            revert("not ready");
+            return _deployKavaMagicMimUsdt();
         } else {
             revert("Unsupported chain");
         }
     }
 
-    function _deployKavaMimUsdtVault() private returns (MagicCurveLp vault, MagicCurveLpHarvestor harvestor) {
-        address pool = toolkit.getAddress(block.chainid, "curve.mimusdt.token");
+    function _deployKavaMagicMimUsdt() private returns (MagicCurveLp vault, MagicCurveLpHarvestor harvestor) {
+        address pool = toolkit.getAddress(block.chainid, "curve.mimusdt.pool");
         ICurveRewardGauge gauge = ICurveRewardGauge(toolkit.getAddress(block.chainid, "curve.mimusdt.gauge"));
 
         vault = MagicCurveLp(
@@ -41,10 +48,88 @@ contract MagicCurveLpScript is BaseScript {
         );
 
         ProxyOracle oracle = ProxyOracle(deployer.deploy_ProxyOracle("Kava_MagicCurveLpProxyOracle_MIM_USDT"));
-        //MagicCurveLpOracle oracle = MagicCurveLpOracle(deployer.deploy_MagicCurveLpOracle("Kava_MagicCurveLpOracle_MIM_USDT_Impl_V1"));
-        //oracle.changeOracleImplementation(IOracle(new MagicLevelOracle(vault, ILevelFinanceLiquidityPool(liquidityPool))));
 
+        // TODO: Use the vault version here.
+        //IOracle impl = IOracle(
+        //    new CurveMeta3PoolOracle(
+        //        "MIM3CRV",
+        //        ICurvePool(toolkit.getAddress("mainnet.curve.mim3pool.pool")),
+        //        IAggregator(address(0)), // We can leave out MIM here as it always has a 1 USD (1 MIM) value.
+        //        IAggregator(toolkit.getAddress("mainnet.chainlink.dai")),
+        //        IAggregator(toolkit.getAddress("mainnet.chainlink.usdc")),
+        //        IAggregator(toolkit.getAddress("mainnet.chainlink.usdt"))
+        //    )
+        //);
+        //MagicCurveLpOracle oracle = MagicCurveLpOracle(deployer.deploy_MagicCurveLpOracle("Kava_MagicCurveLpOracle_MIM_USDT_Impl_V1"));
+        //if (oracle.implementation() != impl) {
+        //  oracle.changeOracleImplementation(IOracle(new MagicLevelOracle(vault, ILevelFinanceLiquidityPool(liquidityPool))));
+        //}
+
+        _deployKavaMagicMimUsdtCauldron(vault, oracle, toolkit.getAddress(block.chainid, "aggregators.openocean"));
         _configureVaultStack(pool, vault, gauge, rewardHandler, harvestor, oracle);
+    }
+
+    function _deployKavaMagicMimUsdtCauldron(
+        IERC4626 vault,
+        ProxyOracle oracle,
+        address exchange /* openocean */
+    ) private returns (ISwapperV2 swapper, ILevSwapperV2 levSwapper, ICauldronV4 cauldron) {
+        IBentoBoxV1 box = IBentoBoxV1(toolkit.getAddress(block.chainid, "degenBox"));
+        (swapper, levSwapper) = _deployKavaMimUsdtPoolSwappers(box, vault, exchange);
+
+        vm.startBroadcast();
+        cauldron = CauldronDeployLib.deployCauldronV4(
+            deployer,
+            "Kava_MagicCurveLp_MIM_USDT_Cauldron",
+            box,
+            toolkit.getAddress(block.chainid, "cauldronV4"),
+            IERC20(address(vault)),
+            oracle,
+            "",
+            9800, // 98% ltv
+            100, // 1% interests
+            0, // 0% opening
+            50 // 0.5% liquidation
+        );
+        vm.stopBroadcast();
+
+        deployer.deploy_DegenBoxERC4626Wrapper("Kava_DegenBoxERC4626Wrapper_MagicCurveLP_MIM_USDT", box, vault);
+    }
+
+    function _deployKavaMimUsdtPoolSwappers(
+        IBentoBoxV1 box,
+        IERC4626 vault,
+        address exchange
+    ) private returns (ISwapperV2 swapper, ILevSwapperV2 levSwapper) {
+        address curvePool = toolkit.getAddress(block.chainid, "curve.mimusdt.pool");
+
+        IERC20[] memory tokens = new IERC20[](2);
+        tokens[0] = IERC20(ICurvePool(curvePool).coins(0));
+        tokens[1] = IERC20(ICurvePool(curvePool).coins(1));
+
+        swapper = deployer.deploy_MagicCurveLpSwapper(
+            "Kava_MagicCurveLpSwapper_MIM_USDT",
+            box,
+            vault,
+            IERC20(toolkit.getAddress(block.chainid, "mim")),
+            CurvePoolInterfaceType.IFACTORY_POOL,
+            curvePool,
+            address(0),
+            tokens,
+            exchange
+        );
+
+        levSwapper = deployer.deploy_MagicCurveLpLevSwapper(
+            "Kava_MagicCurveLpLevSwapper_MIM_USDT",
+            box,
+            vault,
+            IERC20(toolkit.getAddress(block.chainid, "mim")),
+            CurvePoolInterfaceType.IFACTORY_POOL,
+            curvePool,
+            address(0),
+            tokens,
+            exchange
+        );
     }
 
     function _configureVaultStack(
---
+++ src/swappers/MagicCurveLpLevSwapper.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+// solhint-disable avoid-low-level-calls
+pragma solidity >=0.8.0;
+
+import "BoringSolidity/interfaces/IERC20.sol";
+import "BoringSolidity/libraries/BoringERC20.sol";
+import "libraries/SafeApprove.sol";
+import "interfaces/IBentoBoxV1.sol";
+import "swappers/CurveLevSwapper.sol";
+import "interfaces/IERC4626.sol";
+import "interfaces/ICurvePool.sol";
+
+contract MagicCurveLpLevSwapper is CurveLevSwapper {
+    using BoringERC20 for IERC20;
+    using SafeApprove for IERC20;
+
+    IERC4626 public immutable vault;
+
+    constructor(
+        IBentoBoxV1 _bentoBox,
+        IERC4626 _vault,
+        IERC20 _mim,
+        CurvePoolInterfaceType _curvePoolInterfaceType,
+        address _curvePool,
+        address _curvePoolDepositor /* Optional Curve Deposit Zapper */,
+        IERC20[] memory _poolTokens,
+        address _zeroXExchangeProxy
+    )
+        CurveLevSwapper(
+            _bentoBox,
+            _vault.asset(),
+            _mim,
+            _curvePoolInterfaceType,
+            _curvePool,
+            _curvePoolDepositor,
+            _poolTokens,
+            _zeroXExchangeProxy
+        )
+    {
+        vault = _vault;
+        curveToken.safeApprove(address(_vault), type(uint256).max);
+    }
+
+    function depositInBentoBox(uint256 amount, address recipient) internal override returns (uint256 shareReturned) {
+        // CurveLP -> MagicCurveLP
+        vault.deposit(amount, address(bentoBox));
+
+        (, shareReturned) = bentoBox.deposit(IERC20(address(vault)), address(bentoBox), recipient, amount, 0);
+    }
+}
