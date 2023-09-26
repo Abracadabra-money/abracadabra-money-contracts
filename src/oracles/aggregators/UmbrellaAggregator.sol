4db10a750284df4b64032066c90af7c60d4b6a98
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2023-09-26 12:48:05 -0400

    ✨ Updated Linea MIM FeeHandler Oracle (#99)

--- src/oracles/aggregators/UmbrellaAggregator.sol
+++ src/oracles/aggregators/UmbrellaAggregator.sol
@@ -4,19 +4,25 @@ pragma solidity >=0.8.0;
 import "interfaces/IAggregator.sol";
 import "interfaces/IUmbrellaFeeds.sol";
 
+interface IUmbrellaRegistry {
+    function getAddress(bytes32 _bytes) external view returns (address);
+}
+
 /// @title UmbrellaAggregator
 /// @notice Wraps umbrella price feed in an aggregator interface
 contract UmbrellaAggregator is IAggregator {
-    IUmbrellaFeeds public immutable feeds;
+    bytes32 public constant FEEDS_KEY_NAME = bytes32("UmbrellaFeeds");
+
+    IUmbrellaRegistry public immutable registry;
     bytes32 public immutable key;
 
-    constructor(bytes32 _key, IUmbrellaFeeds _feeds) {
+    constructor(bytes32 _key, IUmbrellaRegistry _registry) {
         key = _key;
-        feeds = _feeds;
+        registry = _registry;
     }
 
     function decimals() external view override returns (uint8) {
-        return feeds.DECIMALS();
+        return _getFeeds().DECIMALS();
     }
 
     function latestAnswer() external view returns (int256 answer) {
@@ -24,7 +30,11 @@ contract UmbrellaAggregator is IAggregator {
     }
 
     function latestRoundData() public view returns (uint80, int256 answer, uint256, uint256 updatedAt, uint80) {
-        IUmbrellaFeeds.PriceData memory data = feeds.getPriceData(key);
+        IUmbrellaFeeds.PriceData memory data = _getFeeds().getPriceData(key);
         return (0, int256(uint256(data.price)), 0, data.timestamp, 0);
+    }
+
+    function _getFeeds() private view returns (IUmbrellaFeeds) {
+        return IUmbrellaFeeds(registry.getAddress(FEEDS_KEY_NAME));
     }
 }
