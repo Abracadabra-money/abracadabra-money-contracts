3ca51b40fea746eaf763d9bb0e9c4a2c6692da66
0xMerlin <83640350+0xm3rlin@users.noreply.github.com>
2023-10-06 17:31:21 +0200

    Finished initial gm Oracle

--- src/oracles/GmOracleWithAggregator.sol
+++ src/oracles/GmOracleWithAggregator.sol
@@ -4,8 +4,11 @@ pragma solidity >=0.8.0;
 import "interfaces/IOracle.sol";
 import "interfaces/IGmxReader.sol";
 import "interfaces/IAggregator.sol";
+import "BoringSolidity/interfaces/IERC20.sol";
+import "BoringSolidity/libraries/BoringERC20.sol";
 
 contract GmOracleWithAggregator is IOracle {
+    using BoringERC20 for IERC20;
     bytes32 private constant PNL_TYPE = keccak256(abi.encode("MAX_PNL_FACTOR_FOR_TRADERS"));
     IGmxReader public immutable reader;
     IAggregator public immutable indexAggregator;
@@ -24,8 +27,6 @@ contract GmOracleWithAggregator is IOracle {
         IGmxReader _reader,
         IAggregator _indexToken,
         IAggregator _shortToken,
-        uint256 _expansionFactorIndex,
-        uint256 _expansionFactorShort,
         address _market,
         address _dataStore,
         string memory _desc
@@ -33,11 +34,13 @@ contract GmOracleWithAggregator is IOracle {
         reader = _reader;
         indexAggregator = _indexToken;
         shortAggregator = _shortToken;
-        expansionFactorIndex = _expansionFactorIndex;
-        expansionFactorShort = _expansionFactorShort;
         dataStore = _dataStore;
         Market.Props memory props = _reader.getMarket(_dataStore, _market);
         (marketToken, indexToken, longToken, shortToken)  = (props.marketToken, props.indexToken, props.longToken, props.shortToken);
+        
+        // GMX uses an internal precision of 1e30
+        expansionFactorIndex = 10 ** (30 - indexAggregator.decimals() - IERC20(indexToken).safeDecimals());
+        expansionFactorShort = 10 ** (30 - shortAggregator.decimals() - IERC20(shortToken).safeDecimals());
         desc = _desc;
     }
 
@@ -48,6 +51,8 @@ contract GmOracleWithAggregator is IOracle {
     function _get() internal view returns (uint256 lpPrice) {
         uint256 indexTokenPrice = uint256(indexAggregator.latestAnswer()) * expansionFactorIndex;
         uint256 shortTokenPrice = uint256(shortAggregator.latestAnswer()) * expansionFactorShort;
+
+        // TODO: consider using the upwards deviation of the index token price e.g. price + deviation
         (int256 price, ) = reader.getMarketTokenPrice(
             dataStore, 
             Market.Props(marketToken, indexToken, longToken, shortToken), 
@@ -57,7 +62,7 @@ contract GmOracleWithAggregator is IOracle {
             PNL_TYPE, 
             false);
 
-        // TODO: fix magic numbers and verify for other gm pairs
+        // GMX uses an internal precision of 1e30
         lpPrice = (1e18 * 1e30) / uint256(price);
     }
 
