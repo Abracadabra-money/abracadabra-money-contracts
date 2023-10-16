5859243c22a1e8a8a89808add066a3a7f8e12137
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2023-10-16 09:31:27 -0400

    removed generated deployer file

--- script/CauldronV4.s.sol
+++ script/CauldronV4.s.sol
@@ -4,17 +4,17 @@ pragma solidity >=0.8.0;
 import "interfaces/IBentoBoxV1.sol";
 import "BoringSolidity/ERC20.sol";
 import "utils/BaseScript.sol";
+import "periphery/CauldronOwner.sol";
+import "cauldrons/CauldronV4.sol";
 
 contract CauldronV4Script is BaseScript {
-    using DeployerFunctions for Deployer;
-
     function deploy() public {
         IBentoBoxV1 degenBox = IBentoBoxV1(toolkit.getAddress(block.chainid, "degenBox"));
         address safe = toolkit.getAddress(block.chainid, "safe.ops");
         ERC20 mim = ERC20(toolkit.getAddress(block.chainid, "mim"));
 
-        CauldronOwner cauldronOwner = deployer.deploy_CauldronOwner(toolkit.prefixWithChainName(block.chainid, "CauldronOwner"), safe, mim);
-        CauldronV4 cauldronV4MC = deployer.deploy_CauldronV4(toolkit.prefixWithChainName(block.chainid, "CauldronV4"), degenBox, mim);
+        CauldronOwner cauldronOwner = CauldronOwner(deploy("CauldronOwner", "CauldronOwner.sol:CauldronOwner", abi.encode(safe, mim)));
+        CauldronV4 cauldronV4MC = CauldronV4(deploy("CauldronV4", "CauldronV4.sol:CauldronV4", abi.encode(degenBox, mim)));
 
         vm.startBroadcast();
         if (!testing()) {
