42de0c3a33b7212ec8d82eebcc8be3d4bb7d4d9e
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2024-02-08 20:10:58 -0500

    deployment

--- src/periphery/EpochBasedRewardDistributor.sol
+++ src/periphery/EpochBasedRewardDistributor.sol
@@ -7,80 +7,105 @@ import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
 interface IEpochBasedStaking {
     function epoch() external view returns (uint256);
 
+    function nextEpoch() external view returns (uint256);
+
+    function rewardData(
+        address token
+    ) external view returns (uint256 periodFinish, uint256 rewardRate, uint256 rewardPerTokenStored, bool exists, uint248 lastUpdateTime);
+
     function notifyRewardAmount(address rewardToken, uint256 amount, uint minRemainingTime) external;
+
+    function rewardTokensLength() external view returns (uint);
+
+    function rewardTokens(uint index) external view returns (address);
 }
 
+/// @notice Distribute rewards to staking contracts based on epochs
+/// Amounts deposited to this contract are distributed to staking contracts
+/// only on the next epoch
 contract EpochBasedRewardDistributor is OperatableV2 {
     using SafeTransferLib for address;
-    
-    event LogRewardSet(address indexed reward, uint256 amount);
+
+    event LogRewardAdded(address indexed reward, uint256 amount);
     event LogWithdraw(address indexed token, address indexed to, uint256 amount);
     event LogDistributed(uint256 epoch);
+    event LogMinRemainingTimeSet(uint256 previous, uint256 current);
 
+    error ErrInvalidRewardToken();
     error ErrNotReady();
 
     address public immutable staking;
 
-    uint256 public lastEpoch;
-    address[] public rewards;
-    mapping(address => uint256) public amount;
+    uint256 public epoch;
     uint256 public minRemainingTime;
+
+    mapping(address => uint256) public balanceOf;
 
     constructor(address _staking, uint256 _minRemainingTime, address _owner) OperatableV2(_owner) {
         staking = _staking;
         minRemainingTime = _minRemainingTime;
+        epoch = IEpochBasedStaking(_staking).nextEpoch();
     }
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    /// VIEWS
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
     function ready() public view returns (bool) {
-        return lastEpoch < IEpochBasedStaking(staking).epoch();
+        return epoch <= IEpochBasedStaking(staking).epoch();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    /// OPERATORS
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    function deposit(address _token, uint256 _amount) external onlyOperators {
+        (, , , bool exists, ) = IEpochBasedStaking(staking).rewardData(_token);
+        if (!exists) {
+            revert ErrInvalidRewardToken();
+        }
+
+        _token.safeTransferFrom(msg.sender, address(this), _amount);
+        balanceOf[_token] += _amount;
+
+        _token.safeApprove(staking, balanceOf[_token]);
+
+        emit LogRewardAdded(_token, _amount);
+    }
+
+    function distribute() external onlyOperators {
+        if (!ready()) {
+            revert ErrNotReady();
+        }
+
+        uint256 rewardLength = IEpochBasedStaking(staking).rewardTokensLength();
+
+        for (uint256 i = 0; i < rewardLength; i++) {
+            address reward = IEpochBasedStaking(staking).rewardTokens(i);
+            uint256 rewardAmount = balanceOf[reward];
+
+            delete balanceOf[reward];
+
+            if (rewardAmount > 0) {
+                IEpochBasedStaking(staking).notifyRewardAmount(reward, rewardAmount, minRemainingTime);
+            }
+        }
+
+        epoch = IEpochBasedStaking(staking).nextEpoch();
+        emit LogDistributed(epoch);
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     /// ADMIN
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
-    /// @notice Set reward token and amount distributed on each epoch
-    /// @param _reward Reward token address
-    /// @param _amount Reward amount, use 0 to remove
-    function setReward(address _reward, uint256 _amount) external onlyOwner {
-        for (uint256 i = 0; i < rewards.length; i++) {
-            if (rewards[i] == _reward) {
-                rewards[i] = rewards[rewards.length - 1];
-                rewards.pop();
-                break;
-            }
-        }
-
-        if (_amount > 0) {
-            rewards.push(_reward);
-        }
-        amount[_reward] = _amount;
-
-        emit LogRewardSet(_reward, _amount);
-    }
-
     function withdraw(address _token, address _to, uint256 _amount) external onlyOwner {
         _token.safeTransfer(_to, _amount);
+        balanceOf[_token] = _token.balanceOf(address(this));
         emit LogWithdraw(_token, _to, _amount);
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    /// OPERATORS
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-
-    function distributeRewards(address _staking) external onlyOperators {
-        if(!ready()) {
-            revert ErrNotReady();
-        }
-
-        for (uint256 i = 0; i < rewards.length; i++) {
-            address reward = rewards[i];
-            uint256 rewardAmount = amount[reward];
-            IEpochBasedStaking(_staking).notifyRewardAmount(reward, rewardAmount, minRemainingTime);
-        }
-
-        lastEpoch = IEpochBasedStaking(_staking).epoch();
-        emit LogDistributed(lastEpoch);
+    function setMinRemainingTime(uint256 _minRemainingTime) external onlyOwner {
+        emit LogMinRemainingTimeSet(minRemainingTime, _minRemainingTime);
+        minRemainingTime = _minRemainingTime;
     }
-    
 }
