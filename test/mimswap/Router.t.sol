669838ee637b46906a07b50df8b337cbf7626368
0xmDreamy <0xmDreamy@proton.me>
2024-02-19 14:07:33 +0100

    add some fuzz tests for Router

--- test/mimswap/Router.t.sol
+++ test/mimswap/Router.t.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.0;
 
+import {Test} from "forge-std/Test.sol";
 import "utils/BaseTest.sol";
 import {ERC20Mock} from "openzeppelin-contracts/mocks/ERC20Mock.sol";
+import {IERC20} from "openzeppelin-contracts/interfaces/IERC20.sol";
 import {WETH} from "solady/tokens/WETH.sol";
 import {FeeRateModel} from "/mimswap/auxiliary/FeeRateModel.sol";
 import {IWETH} from "interfaces/IWETH.sol";
+import {IMagicLP} from "/mimswap/interfaces/IMagicLP.sol";
 import {MagicLP} from "/mimswap/MagicLP.sol";
 import {Router} from "/mimswap/periphery/Router.sol";
 
@@ -71,5 +74,96 @@ contract RouterTest is BaseTest {
 
         vm.prank(alice);
         router.swapTokensForTokens(alice, 1 ether, path, directions, 1, type(uint256).max);
+    }
+}
+
+contract RouterUnitTest is Test {
+    address weth;
+    Router router;
+
+    function setUp() public {
+        weth = makeAddr("WETH");
+        router = new Router(IWETH(weth));
+    }
+
+    struct PathDataEntry {
+        address lp;
+        address baseToken;
+        address quoteToken;
+        bool sellQuote;
+        uint256 amountOut;
+    }
+
+    /// forge-config: default.fuzz.runs = 65536
+    function testEnsureDeadline(
+        address lp,
+        address to,
+        uint256 amountIn,
+        uint256 minimumOut,
+        uint256 deadline,
+        uint256 afterDeadLine
+    ) public {
+        afterDeadLine = _bound(afterDeadLine, deadline == type(uint256).max ? deadline : deadline + 1, type(uint256).max);
+        vm.warp(afterDeadLine);
+        vm.expectRevert();
+        router.sellQuoteTokensForTokens(lp, to, amountIn, minimumOut, deadline);
+    }
+
+    function testSwapRouter(address to, uint256 amountIn, PathDataEntry[] calldata pathData, uint256 minimumOut) public {
+        vm.assume(pathData.length > 0 && pathData.length <= 256);
+
+        address[] memory path = new address[](pathData.length);
+        uint256 directions = 0;
+
+        address inToken = pathData[0].sellQuote ? pathData[0].quoteToken : pathData[0].baseToken;
+        // Assume inToken not VM_ADDRESS nor precompile
+        vm.assume(inToken != VM_ADDRESS && uint160(inToken) > 0xff);
+
+        vm.expectCall(inToken, abi.encodeCall(IERC20.transferFrom, (address(this), pathData[0].lp, amountIn)), 1);
+        // Ensure code on inToken
+        vm.etch(inToken, "");
+        vm.mockCall(inToken, abi.encodeCall(IERC20.transferFrom, (address(this), pathData[0].lp, amountIn)), "");
+
+        for (uint256 i = 0; i < pathData.length; ++i) {
+            PathDataEntry memory entry = pathData[i];
+            // Assume lp not VM_ADDRESS nor precompile
+            vm.assume(entry.lp != VM_ADDRESS && uint160(entry.lp) > 0xff);
+            // Assume different LP addresses --- to avoid collisions in mockCall/expectCall
+            for (uint256 j = 0; j < i; ++j) {
+                vm.assume(path[j] != entry.lp);
+            }
+
+            bool last = i == pathData.length - 1;
+
+            path[i] = entry.lp;
+
+            bytes memory sellCallEncoded = abi.encodeWithSelector(
+                entry.sellQuote ? MagicLP.sellQuote.selector : IMagicLP.sellBase.selector,
+                (last ? to : pathData[i + 1].lp)
+            );
+            vm.expectCall(entry.lp, sellCallEncoded, 1);
+            // Ensure code on lp
+            vm.etch(entry.lp, "");
+            vm.mockCall(entry.lp, sellCallEncoded, abi.encode(entry.amountOut));
+
+            vm.mockCall(entry.lp, abi.encodeCall(IMagicLP._BASE_TOKEN_, ()), abi.encode(entry.baseToken));
+            vm.mockCall(entry.lp, abi.encodeCall(IMagicLP._QUOTE_TOKEN_, ()), abi.encode(entry.quoteToken));
+
+            // Directions are stored in reverse
+            directions |= pathData[pathData.length - i - 1].sellQuote ? 1 : 0;
+            if (!last) {
+                directions <<= 1;
+            }
+        }
+
+        uint256 expectedOut = pathData[pathData.length - 1].amountOut;
+
+        if (expectedOut < minimumOut) {
+            vm.expectRevert();
+            router.swapTokensForTokens(to, amountIn, path, directions, minimumOut, type(uint256).max);
+        } else {
+            uint256 outAmount = router.swapTokensForTokens(to, amountIn, path, directions, minimumOut, type(uint256).max);
+            assertEq(outAmount, expectedOut);
+        }
     }
 }
