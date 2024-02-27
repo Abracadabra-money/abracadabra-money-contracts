18979eb00e5358ba92ccf96642e455889640be3f
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2024-02-27 15:33:41 -0500

    No need to emit and event as blast point already does

--- src/blast/libraries/BlastPoints.sol
+++ src/blast/libraries/BlastPoints.sol
@@ -4,12 +4,9 @@ pragma solidity >=0.8.0;
 import {IBlastPoints} from "interfaces/IBlast.sol";
 
 library BlastPoints {
-    event LogBlastPointsOperatorConfigured(address indexed contractAddress, address indexed operator);
-
     IBlastPoints constant BLAST_POINTS = IBlastPoints(0x2fc95838c71e76ec69ff817983BFf17c710F34E0);
 
     function configurePointsOperator(address _pointsOperator) internal {
         BLAST_POINTS.configurePointsOperator(_pointsOperator);
-        emit LogBlastPointsOperatorConfigured(address(this), _pointsOperator);
     }
 }
