a128528b864b177556d8a7ca31866bc1ae872680
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2024-03-04 14:21:33 -0500

    M-02 and L-04

--- src/mimswap/periphery/Factory.sol
+++ src/mimswap/periphery/Factory.sol
@@ -28,7 +28,7 @@ contract Factory is Owned {
 
     error ErrInvalidUserPoolIndex();
     error ErrZeroAddress();
-    
+
     address public implementation;
     IFeeRateModel public maintainerFeeRateModel;
 
@@ -63,7 +63,7 @@ contract Factory is Owned {
     //////////////////////////////////////////////////////////////////////////////////////
 
     function predictDeterministicAddress(
-        address sender,
+        address creator,
         address baseToken_,
         address quoteToken_,
         uint256 lpFeeRate_,
@@ -71,16 +71,22 @@ contract Factory is Owned {
         uint256 k_
     ) public view returns (address) {
         return
-            LibClone.predictDeterministicAddress(implementation, _computeSalt(sender, baseToken_, quoteToken_, lpFeeRate_, i_, k_), address(this));
+            LibClone.predictDeterministicAddress(
+                implementation,
+                _computeSalt(creator, baseToken_, quoteToken_, lpFeeRate_, i_, k_),
+                address(this)
+            );
     }
 
     function create(address baseToken_, address quoteToken_, uint256 lpFeeRate_, uint256 i_, uint256 k_) external returns (address clone) {
-        bytes32 salt = _computeSalt(msg.sender, baseToken_, quoteToken_, lpFeeRate_, i_, k_);
+        address creator = tx.origin;
+
+        bytes32 salt = _computeSalt(creator, baseToken_, quoteToken_, lpFeeRate_, i_, k_);
         clone = LibClone.cloneDeterministic(address(implementation), salt);
         IMagicLP(clone).init(address(baseToken_), address(quoteToken_), lpFeeRate_, address(maintainerFeeRateModel), i_, k_);
 
-        emit LogCreated(clone, baseToken_, quoteToken_, msg.sender, lpFeeRate_, maintainerFeeRateModel, i_, k_);
-        _addPool(msg.sender, baseToken_, quoteToken_, clone);
+        emit LogCreated(clone, baseToken_, quoteToken_, creator, lpFeeRate_, maintainerFeeRateModel, i_, k_);
+        _addPool(creator, baseToken_, quoteToken_, clone);
     }
 
     //////////////////////////////////////////////////////////////////////////////////////
