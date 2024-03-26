7d7184ed52607d38113546b921156fb957c7670c
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2024-03-26 15:48:58 -0400

    wip testing

---
+++ test/MigrationCauldrons.t.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "utils/BaseTest.sol";
+import "script/MigrationCauldrons.s.sol";
+import {CauldronTestLib} from "./utils/CauldronTestLib.sol";
+import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
+import {BoringOwnable} from "BoringSolidity/BoringOwnable.sol";
+
+contract MigrationCauldronsTest is BaseTest {
+    using SafeTransferLib for address;
+    MigrationCauldronsScript script;
+
+    function setUp() public override {
+        fork(ChainId.Mainnet, 19520183);
+        super.setUp();
+
+        script = new MigrationCauldronsScript();
+        script.setTesting(true);
+        script.deploy();
+    }
+
+    function test() public {
+        Deployement[] memory deployement = script.getDeployments();
+        uint topUpAmount = 100_000_000 ether;
+
+        for (uint256 i = 0; i < deployement.length; i++) {
+            Deployement memory d = deployement[i];
+            uint256 decimals = d.decimals;
+
+            console2.log("-> %s", d.name);
+
+            ICauldronV2 cauldron = ICauldronV2(d.cauldron);
+            ICauldronV2 masterContract = ICauldronV2(d.cauldron).masterContract();
+            IBentoBoxV1 box = IBentoBoxV1(cauldron.bentoBox());
+
+            pushPrank(BoringOwnable(address(masterContract)).owner());
+            box.whitelistMasterContract(address(masterContract), true);
+
+            address mim = address(cauldron.magicInternetMoney());
+            deal(mim, address(alice), topUpAmount, true);
+
+            pushPrank(alice);
+            mim.safeTransfer(address(box), topUpAmount);
+            box.deposit(IERC20(mim), address(box), address(d.cauldron), topUpAmount, 0);
+            deal(d.token, alice, 100 * (10 ** decimals), true);
+
+            CauldronTestLib.depositAndBorrow(
+                box,
+                ICauldronV2(d.cauldron),
+                address(masterContract),
+                IERC20(d.token),
+                alice,
+                100 * (10 ** decimals),
+                50
+            );
+            popPrank();
+        }
+    }
+}
--- test/utils/CauldronTestLib.sol
+++ test/utils/CauldronTestLib.sol
@@ -5,8 +5,11 @@ import {IBentoBoxV1} from "interfaces/IBentoBoxV1.sol";
 import {IERC20} from "BoringSolidity/interfaces/IERC20.sol";
 import {ICauldronV2} from "interfaces/ICauldronV2.sol";
 import {IOracle} from "interfaces/IOracle.sol";
+import {Toolkit} from "utils/Toolkit.sol";
 
 library CauldronTestLib {
+    Toolkit constant toolkit = Toolkit(address(bytes20(uint160(uint256(keccak256("toolkit"))))));
+
     function depositAndBorrow(
         IBentoBoxV1 box,
         ICauldronV2 cauldron,
@@ -21,10 +24,33 @@ library CauldronTestLib {
         collateral.approve(address(box), amount);
         (, borrowAmount) = box.deposit(collateral, account, account, amount, 0);
         cauldron.addCollateral(account, false, borrowAmount);
+        address mim = address(cauldron.magicInternetMoney());
 
-        amount = (1e18 * amount) / cauldron.oracle().peekSpot("");
+        amount = (1e18 * amount) / cauldron.oracle().peekSpot(cauldron.oracleData());
         borrowAmount = (amount * percentBorrow) / 100;
+
+        uint256 mlb = box.toAmount(IERC20(mim), box.balanceOf(IERC20(mim), address(cauldron)), false);
+
+        if (borrowAmount > mlb) {
+            revert(
+                string.concat(
+                    "CauldronTestLib: Not enough MIM to borrow. Trying to borrow ",
+                    toolkit.formatDecimals(borrowAmount),
+                    " MIM but MLB is ",
+                    toolkit.formatDecimals(mlb),
+                    " MIM."
+                )
+            );
+        }
+
         cauldron.borrow(account, borrowAmount);
+
+        uint256 mimAmount = box.toAmount(IERC20(mim), box.balanceOf(IERC20(mim), address(account)), false);
+        if (borrowAmount > mimAmount) {
+            borrowAmount = mimAmount;
+        }
+
+        require(borrowAmount > 0, "CauldronTestLib: Borrow amount is zero");
 
         box.withdraw(cauldron.magicInternetMoney(), account, account, borrowAmount, 0);
     }
