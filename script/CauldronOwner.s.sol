2d3a154cc4d13d7110008a921fce01dfb9bffa04
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2024-05-15 13:26:25 -0400

    deployment script

--- script/CauldronOwner.s.sol
+++ script/CauldronOwner.s.sol
@@ -3,19 +3,25 @@ pragma solidity >=0.8.0;
 
 import {BaseScript} from "utils/BaseScript.sol";
 import {CauldronOwner} from "periphery/CauldronOwner.sol";
+import {CauldronRegistry} from "periphery/CauldronRegistry.sol";
 
 contract CauldronOwnerScript is BaseScript {
-    function deploy() public {
+    function deploy() public returns (CauldronOwner cauldronOwner) {
         address safe = toolkit.getAddress(block.chainid, "safe.ops");
         address mim = toolkit.getAddress(block.chainid, "mim");
+        address registry = toolkit.getAddress(block.chainid, "cauldronRegistry");
 
         vm.startBroadcast();
-        CauldronOwner cauldronOwner = CauldronOwner(deploy("CauldronOwner", "CauldronOwner.sol:CauldronOwner", abi.encode(safe, mim)));
+        cauldronOwner = CauldronOwner(deploy("CauldronOwner", "CauldronOwner.sol:CauldronOwner", abi.encode(safe, mim, tx.origin)));
 
         if (!testing()) {
             if (cauldronOwner.owner() == tx.origin) {
-                cauldronOwner.grantRoles(tx.origin, cauldronOwner.ROLE_OPERATOR());
-                cauldronOwner.grantRoles(0x8764F421AB0C682b4Ba1d7e269C09187c1EfbFAF, cauldronOwner.ROLE_CHANGE_BORROW_LIMIT());
+                if (!cauldronOwner.hasAnyRole(tx.origin, cauldronOwner.ROLE_OPERATOR())) {
+                    cauldronOwner.grantRoles(tx.origin, cauldronOwner.ROLE_OPERATOR());
+                }
+                if(cauldronOwner.registry() != CauldronRegistry(registry)) {
+                    cauldronOwner.setRegistry(CauldronRegistry(registry));
+                }
                 cauldronOwner.transferOwnership(safe);
             }
         }
