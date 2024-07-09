9c81e50176cf39c689c266d7e6818dec6c28ee46
0xCalibur <0xCalibur@protonmail.com>
2024-07-09 09:01:59 -0400

    test

--- test/RewardDistributors.t.sol
+++ test/RewardDistributors.t.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.13;
 
 import "utils/BaseTest.sol";
 import "script/RewardDistributors.s.sol";
+import {Owned} from "solmate/auth/Owned.sol";
+import {OperatableV2} from "mixins/OperatableV2.sol";
 
 contract RewardDistributorsTest is BaseTest {
     EpochBasedRewardDistributor epochDistributor;
@@ -10,6 +12,8 @@ contract RewardDistributorsTest is BaseTest {
 
     address constant multiRewardsStaking = 0xc30911b52b5752447aB08615973e434c801CD652; // magicLP mim/usdc stip2
     address constant epochBasedStaking = 0x98164deD88e2a4862BdE8E7D4B831a6e3BE02D0A; // mim saving rate
+
+    address vault;
 
     function setUp() public override {
         fork(ChainId.Arbitrum, 230248146);
@@ -19,5 +23,66 @@ contract RewardDistributorsTest is BaseTest {
         script.setTesting(true);
 
         (epochDistributor, multiDistributor) = script.deploy();
+
+        pushPrank(Owned(epochBasedStaking).owner());
+        OperatableV2(epochBasedStaking).setOperator(address(epochDistributor), true);
+        popPrank();
+
+        pushPrank(Owned(multiRewardsStaking).owner());
+        OperatableV2(multiRewardsStaking).setOperator(address(multiDistributor), true);
+        popPrank();
+
+        vault = makeAddr("safe.rewards");
+
+        pushPrank(epochDistributor.owner());
+        epochDistributor.setOperator(alice, true);
+        epochDistributor.setVault(vault);
+        popPrank();
+
+        pushPrank(multiDistributor.owner());
+        multiDistributor.setOperator(alice, true);
+        multiDistributor.setVault(vault);
+        popPrank();
     }
+
+    function testNotAllowedOperator() public {
+        vm.expectRevert(abi.encodeWithSignature("NotAllowedOperator()"));
+        epochDistributor.distribute(epochBasedStaking);
+
+        vm.expectRevert(abi.encodeWithSignature("NotAllowedOperator()"));
+        multiDistributor.distribute(multiRewardsStaking);
+    }
+
+    function testDistributorWithoutConfiguration() public {
+        pushPrank(alice);
+        epochDistributor.distribute(epochBasedStaking);
+        multiDistributor.distribute(multiRewardsStaking);
+        popPrank();
+    }
+
+    function testDistributeUnsupportedRewards() public {
+        pushPrank(epochDistributor.owner());
+        epochDistributor.setOperator(alice, true);
+        epochDistributor.setVault(vault);
+        popPrank();
+
+        pushPrank(multiDistributor.owner());
+        multiDistributor.setOperator(alice, true);
+        multiDistributor.setVault(vault);
+        popPrank();
+
+        pushPrank(alice);
+        epochDistributor.distribute(epochBasedStaking);
+        multiDistributor.distribute(multiRewardsStaking);
+        popPrank();
+    }
+
+    function testDistributeMissingRewardsOnVault() public {
+        pushPrank(alice);
+        epochDistributor.distribute(epochBasedStaking);
+        multiDistributor.distribute(multiRewardsStaking);
+        popPrank();
+    }
+
+    function _setupRewards() internal {}
 }
