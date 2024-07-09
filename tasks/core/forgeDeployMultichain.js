985c9d2c58bd4adf12e68a3c6aa4bb42c8b1da7d
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2024-07-09 09:05:49 -0400

    Updated to solc 0.8.26 / shanghai evm & Governance Framework Draft  (#182)

--- tasks/core/forgeDeployMultichain.js
+++ tasks/core/forgeDeployMultichain.js
@@ -9,8 +9,8 @@ module.exports = async function (taskArgs, hre) {
 
     for (const network of taskArgs.networks) {
         changeNetwork(network);
-        
+
         console.log(`Deploying to ${network}...`);
-        await hre.run("forge-deploy", { network, script: taskArgs.script, broadcast: taskArgs.broadcast, verify: taskArgs.verify, noConfirm: taskArgs.noConfirm, resume: taskArgs.resume });
+        await hre.run("forge-deploy", { network, script: taskArgs.script, broadcast: taskArgs.broadcast, verify: taskArgs.verify, noConfirm: taskArgs.noConfirm });
     }
 }
