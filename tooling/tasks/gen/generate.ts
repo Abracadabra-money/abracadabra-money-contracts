374db7336cb7c3e2264a9c87adf2611559e3ba7a
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2024-07-18 00:40:37 -0400

    ported gen task

--- tooling/tasks/gen/generate.ts
+++ tooling/tasks/gen/generate.ts
@@ -2,8 +2,11 @@ import type { TaskArgs, TaskFunction, TaskMeta, Tooling } from '../../types';
 import path from 'path';
 import fs from 'fs';
 import { getFolders } from '../utils';
-import { input } from '@inquirer/prompts';
+import { input, confirm } from '@inquirer/prompts';
+import select from '@inquirer/select';
 import Handlebars from 'handlebars';
+import { readdir } from "node:fs/promises";
+import { Glob } from 'bun';
 
 export const meta: TaskMeta = {
     name: 'gen:gen',
@@ -21,7 +24,7 @@ export const task: TaskFunction = async (taskArgs: TaskArgs, tooling: Tooling) =
     const chainIdEnum = Object.keys(tooling.config.networks).reduce((acc, network) => {
         const capitalizedNetwork = network.charAt(0).toUpperCase() + network.slice(1);
         return { ...acc, [tooling.config.networks[network].chainId]: capitalizedNetwork };
-    }, {});
+    }, {}) as { [key: string]: string };
 
     let answers: any = {};
 
@@ -33,17 +36,26 @@ export const task: TaskFunction = async (taskArgs: TaskArgs, tooling: Tooling) =
         ...await getFolders(utilsFolder)
     ];
 
-    const defaultPostDefaultQuestions = [
-        {
-            message: 'Destination Folder',
-            type: 'list',
-            name: 'destination',
-            choices: destinationFolders.map(folder => folder)
-        },
-    ];
+    const glob = new Glob("*.s.sol");
+    const scriptFiles = (await Array.fromAsync(glob.scan(tooling.config.foundry.script))).map(f => {
+        const name = path.basename(f).replace(".s.sol", "")
+        return {
+            name,
+            value: name
+        }
+    });
 
-    switch ((taskArgs.template as string[])[0] as string) {
-        case 'script':
+    const promptDestinationFolder = async () => {
+        return await select({
+            message: 'Destination Folder',
+            choices: destinationFolders.map(folder => ({ name: folder, value: folder }))
+        });
+    }
+
+    taskArgs.template = (taskArgs.template as string[])[0] as string;
+
+    switch (taskArgs.template) {
+        case 'script': {
             const scriptName = await input({ message: 'Script Name' });
             const filename = await input({ message: 'Filename', default: `${scriptName}.s.sol` });
 
@@ -51,118 +63,105 @@ export const task: TaskFunction = async (taskArgs: TaskArgs, tooling: Tooling) =
             answers.filename = filename
             answers.destination = tooling.config.foundry.script;
             break;
-        //case 'interface':
-        //    answers = await inquirer.prompt([
-        //        { name: 'interfaceName', message: 'Interface Name' },
-        //        {
-        //            name: 'filename',
-        //            message: 'Filename',
-        //            default: answers => `${answers.interfaceName}.sol`
-        //        }
-        //    ]);
-        //    answers.destination = `${tooling.config.foundry.src}/interfaces`;
-        //    break;
-        //case 'contract':
-        //    answers = await inquirer.prompt([
-        //        { name: 'contractName', message: 'Contract Name' },
-        //        {
-        //            name: 'filename',
-        //            message: 'Filename',
-        //            default: answers => `${answers.contractName}.sol`
-        //        },
-        //        {
-        //            name: 'operatable', type: 'confirm',
-        //            message: 'Operatable?',
-        //            default: false
-        //        },
-        //        ...defaultPostDefaultQuestions
-        //    ]);
-        //    break;
-        //case 'blast-wrapped':
-        //    answers = await inquirer.prompt([
-        //        { name: 'contractName', message: 'Contract Name' },
-        //        {
-        //            name: 'filename',
-        //            message: 'Filename',
-        //            default: answers => `${answers.contractName}.sol`
-        //        },
-        //        ...defaultPostDefaultQuestions
-        //    ]);
-        //    break;
+        }
+        case 'interface': {
+            const interfaceName = await input({ message: 'Interface Name' });
+            const filename = await input({ message: 'Filename', default: `${interfaceName}.sol` });
 
-        //case 'test':
-        //    const scriptFiles = (await glob(`${tooling.config.foundry.script}/*.s.sol`)).map(f => path.basename(f).replace(".s.sol", ""));
-        //    const modes = [{
-        //        name: "Simple",
-        //        value: "simple"
-        //    },
-        //    {
-        //        name: "Multi (base test-contract + per-suite-test-contract)",
-        //        value: "multi"
-        //    }];
+            answers.interfaceName = interfaceName;
+            answers.filename = filename
+            answers.destination = `${tooling.config.foundry.src}/interfaces`;
+            break;
+        }
+        case 'contract': {
+            const contractName = await input({ message: 'Contract Name' });
+            const filename = await input({ message: 'Filename', default: `${contractName}.sol` });
+            const operatable = await confirm({ message: 'Operatable?', default: false });
+            const destination = await promptDestinationFolder();
 
-        //    answers = await inquirer.prompt([
-        //        { name: 'testName', message: 'Test Name' },
-        //        {
-        //            message: 'Script',
-        //            type: 'list',
-        //            name: 'scriptName',
-        //            choices: ["(None)", ...scriptFiles],
-        //            default: answers => answers.testName
-        //        },
-        //        {
-        //            message: 'Type',
-        //            type: 'list',
-        //            name: 'mode',
-        //            choices: modes
-        //        },
-        //        {
-        //            message: 'Network',
-        //            type: 'list',
-        //            name: 'network',
-        //            choices: networks.map(network => ({ name: network.name, value: { chainId: `ChainId.${chainIdEnum[network.chainId]}`, name: network.name } }))
-        //        },
-        //        { name: 'blockNumber', message: 'Block', default: "latest" },
-        //        {
-        //            name: 'filename',
-        //            message: 'Filename',
-        //            default: answers => `${answers.testName}.t.sol`
-        //        }
-        //    ]);
-        //    answers.destination = tooling.config.foundry.test;
+            answers.contractName = contractName;
+            answers.filename = filename
+            answers.destination = destination;
+            answers.operatable = operatable;
+            break;
+        }
+        case 'blast-wrapped': {
+            const contractName = await input({ message: 'Contract Name' });
+            const filename = await input({ message: 'Filename', default: `${contractName}.sol` });
+            const destination = await promptDestinationFolder();
+            answers.contractName = contractName;
+            answers.filename = filename
+            answers.destination = destination;
+            break;
+        }
+        case 'test': {
+            const modes = [{
+                name: "Simple",
+                value: "simple"
+            },
+            {
+                name: "Multi (base test-contract + per-suite-test-contract)",
+                value: "multi"
+            }];
 
-        //    if (answers.mode === "multi") {
-        //        taskArgs.template = "test-multi";
-        //    }
+            const testName = await input({ message: 'Test Name' });
+            const scriptName = await select({
+                message: 'Script',
+                choices: [{ name: "(None)", value: "(None)" }, ...scriptFiles],
+                default: testName
+            });
+            const mode = await select({
+                message: 'Type',
+                choices: modes
+            });
+            const network = await select({
+                message: 'Network',
+                choices: networks.map(network => ({ name: network.name, value: { chainId: `ChainId.${chainIdEnum[network.chainId]}`, name: network.name } }))
+            });
+            const blockNumber = await input({ message: 'Block', default: "latest" });
+            const filename = await input({ message: 'Filename', default: `${testName}.t.sol` });
 
-        //    if (answers.scriptName === "(None)") {
-        //        answers.scriptName = undefined;
-        //    }
+            answers.testName = testName;
+            answers.scriptName = scriptName;
+            answers.mode = mode;
+            answers.network = network;
+            answers.blockNumber = blockNumber;
+            answers.filename = filename;
+            answers.destination = tooling.config.foundry.test;
 
-        //    if (answers.scriptName) {
-        //        const solidityCode = fs.readFileSync(`${tooling.config.foundry.script}/${answers.scriptName}.s.sol`, 'utf8');
-        //        const regex = /function deploy\(\) public returns \((.*?)\)/;
+            if (answers.mode === "multi") {
+                taskArgs.template = "test-multi";
+            }
 
-        //        const matches = solidityCode.match(regex);
+            if (answers.scriptName === "(None)") {
+                answers.scriptName = undefined;
+            }
 
-        //        if (matches && matches.length > 1) {
-        //            const returnValues = matches[1].trim();
-        //            answers.deployVariables = returnValues.split(',').map(value => value.trim());
-        //            answers.deployReturnValues = returnValues.split(',').map(value => value.trim().split(' ')[1]);
-        //        }
-        //    }
+            if (answers.scriptName) {
+                const solidityCode = fs.readFileSync(`${tooling.config.foundry.script}/${answers.scriptName}.s.sol`, 'utf8');
+                const regex = /function deploy\(\) public returns \((.*?)\)/;
 
-        //    if (answers.blockNumber == "latest") {
-        //        tooling.changeNetwork(answers.network.name.toString().toLowerCase());
-        //        answers.blockNumber = await tooling.getProvider().getBlockNumber();
-        //        console.log(`Using Block: ${answers.blockNumber}`);
-        //    }
+                const matches = solidityCode.match(regex);
 
-        //    answers.blockNumber = parseInt(answers.blockNumber);
-        //    break;
+                if (matches && matches.length > 1) {
+                    const returnValues = matches[1].trim();
+                    answers.deployVariables = returnValues.split(',').map(value => value.trim());
+                    answers.deployReturnValues = returnValues.split(',').map(value => value.trim().split(' ')[1]);
+                }
+            }
+
+            if (answers.blockNumber == "latest") {
+                tooling.changeNetwork(answers.network.name.toString().toLowerCase());
+                answers.blockNumber = await tooling.getProvider().getBlockNumber();
+                console.log(`Using Block: ${answers.blockNumber}`);
+            }
+
+            answers.blockNumber = parseInt(answers.blockNumber);
+            break;
+        }
         default:
             console.error(`Template ${taskArgs.template} does not exist`);
-        process.exit(1);
+            process.exit(1);
     }
 
     // Compile the template
