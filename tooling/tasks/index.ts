ba4c25936ef0afb5e6312a9b22729a1b7b28aa30
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2024-07-18 10:52:06 -0400

    verify task

--- tooling/tasks/index.ts
+++ tooling/tasks/index.ts
@@ -5,11 +5,14 @@ import * as CheckConsoleLogTask from './core/check-console-log';
 import * as ForgeDeployTask from './core/forge-deploy';
 import * as ForgeDeployMultichainTask from './core/forge-deploy-multichain';
 import * as PostDeployTask from './core/post-deploy';
+import * as VerifyTask from './core/verify';
 import * as WithdrawFeesTask from './lz/withdraw-fees';
 import * as CheckPathTasks from './lz/check-paths';
 import * as BridgeTask from './lz/bridge';
 import * as ChangeOwnersTask from './lz/change-owners';
 import * as CheckMimTotalSupplyTask from './lz/check-mim-total-supply';
+import * as DeployOFTV2Task from './lz/deploy-oftv2';
+import * as DeployPrecrimeTask from './lz/deploy-precrime';
 import * as CauldronInfoTask from './cauldrons/info';
 import * as CauldronGnosisSetFeeTooTask from './cauldrons/gnosis-set-feeto';
 import * as GenerateMerkleAccountAmountTask from './gen/merkle-account-amount';
@@ -23,11 +26,14 @@ export const tasks = [
     ForgeDeployTask,
     ForgeDeployMultichainTask,
     PostDeployTask,
+    VerifyTask,
     WithdrawFeesTask,
     CheckPathTasks,
     BridgeTask,
     ChangeOwnersTask,
     CheckMimTotalSupplyTask,
+    DeployOFTV2Task,
+    DeployPrecrimeTask,
     CauldronInfoTask,
     CauldronGnosisSetFeeTooTask,
     GenerateMerkleAccountAmountTask,
