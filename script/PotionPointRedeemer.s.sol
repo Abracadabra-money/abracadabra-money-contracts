803430bc69defb6c65e93a61fffba539bb63c20d
0xMerlin <83640350+0xm3rlin@users.noreply.github.com>
2024-07-29 22:18:52 +0900

    Deployment Potion Points (#195)
    * Deployment Potion Points
    
    ---------
    
    Co-authored-by: 0xCalibur <92554750+0xCalibur@users.noreply.github.com>

--- script/PotionPointRedeemer.s.sol
+++ script/PotionPointRedeemer.s.sol
@@ -6,7 +6,7 @@ import "utils/BaseScript.sol";
 contract PotionPointRedeemerScript is BaseScript {
     function deploy() public {
         vm.startBroadcast();
-        deploy("PotionPointRedeemer", "PotionPointRedeemer.sol:PotionPointRedeemer", abi.encode(tx.origin));
+        deploy("PotionPointRedeemer", "PotionPointRedeemer.sol:PotionPointRedeemer", abi.encode(9999999999999999999999397,tx.origin));
         vm.stopBroadcast();
     }
 }
