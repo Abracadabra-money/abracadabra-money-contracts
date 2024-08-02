a3930c6dec5721d32b04e3026202035830897a27
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2024-08-02 16:58:10 -0400

    🔧 Generator: Recursive folder scanning

--- tooling/tasks/utils/index.ts
+++ tooling/tasks/utils/index.ts
@@ -1,21 +1,30 @@
-import { readdir } from "node:fs/promises";
-import path from 'path';
+import {readdir} from "node:fs/promises";
+import path from "path";
 
 type ExecOptions = {
     noThrow?: boolean;
 };
 
 export const getFolders = async (rootDir: string): Promise<string[]> => {
-    return (await readdir(rootDir, { recursive: true, withFileTypes: true }))
-        .filter(file => file.isDirectory())
-        .map(file => `${path.join(file.parentPath, file.name)}`);
-}
+    const entries = await readdir(rootDir, {withFileTypes: true});
+    const folders = await Promise.all(
+        entries.map(async (entry) => {
+            const fullPath = path.join(rootDir, entry.name);
+            if (entry.isDirectory()) {
+                const subFolders = await getFolders(fullPath);
+                return [fullPath, ...subFolders];
+            }
+            return [];
+        })
+    );
+    return folders.flat();
+};
 
 export const exec = async (cmdLike: string[] | string, env: {[key: string]: string}, options?: ExecOptions): Promise<number> => {
     const cmd = Array.isArray(cmdLike) ? cmdLike.join(" ") : cmdLike;
     return new Promise(async (resolve, reject) => {
         const proc = Bun.spawn({
-            cmd: cmd.split(' '),
+            cmd: cmd.split(" "),
             env,
             onExit(_proc, exitCode, _signalCode, _error) {
                 if (exitCode === 0) {
@@ -25,11 +34,11 @@ export const exec = async (cmdLike: string[] | string, env: {[key: string]: stri
                 } else {
                     reject(exitCode);
                 }
-            }
+            },
         });
 
         for await (const chunk of proc.stdout) {
             process.stdout.write(chunk);
         }
     });
-}
+};
