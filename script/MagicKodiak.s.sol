8cb92567aa051f8e003149472362d16f52d7664d
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2024-08-06 14:42:33 -0400

    v1

---
+++ script/MagicKodiak.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+
+import "utils/BaseScript.sol";
+import {LibClone} from "@solady/utils/LibClone.sol";
+import {MagicKodiakVault} from "/tokens/MagicKodiakVault.sol";
+
+contract MagicKodiakScript is BaseScript {
+    bytes32 constant VAULT_SALT = keccak256("MagicKodiakVault");
+
+    function deploy() public returns (MagicKodiakVault instance) {
+        vm.startBroadcast();
+
+        address magicKodiak = deploy(
+            "MagicKodiakVault_BeraHoneyImpl",
+            "MagicKodiakVault.sol:MagicKodiakVault",
+            abi.encode(toolkit.getAddress("kodiak.pools.berahoney"))
+        );
+
+        instance = MagicKodiakVault(deployUsingCreate3("MagicKodiakVault_BeraHoney", VAULT_SALT, LibClone.initCodeERC1967(magicKodiak)));
+        instance.initialize(tx.origin, toolkit.getAddress("kodiak.staking"));
+
+        if (instance.owner() != tx.origin) {
+            revert("owner should be the deployer");
+        }
+
+        instance.grantRoles(tx.origin, instance.ROLE_OPERATOR());
+        vm.stopBroadcast();
+    }
+}
---
+++ src/harvesters/MagicKodiakVaultHarvester.sol
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {OwnableRoles} from "@solady/auth/OwnableRoles.sol";
+import {SafeTransferLib} from "@solady/utils/SafeTransferLib.sol";
+import {FeeCollectable} from "/mixins/FeeCollectable.sol";
+import {IERC4626} from "/interfaces/IERC4626.sol";
+import {IMagicKodiakVault} from "/interfaces/IMagicKodiakVault.sol";
+import {IKodiakVaultStaking} from "/interfaces/IKodiak.sol";
+import {IKodiakVaultV1, IKodiakVaultStaking, IKodiakV1RouterStaking} from "/interfaces/IKodiak.sol";
+
+/// @notice Contract to harvest rewards from the staking contract and distribute them to the vault
+contract MagicKodiakVaultHarvester is OwnableRoles, FeeCollectable {
+    using SafeTransferLib for address;
+
+    error ErrSwapFailed();
+    event LogExchangeRouterChanged(address indexed previous, address indexed current);
+    event LogRouterChanged(address indexed previous, address indexed current);
+    event LogHarvest(uint256 total, uint256 amount, uint256 fee);
+
+    struct SwapInfo {
+        address token;
+        uint256 amount;
+        bytes swapData;
+    }
+
+    uint256 public constant ROLE_OPERATOR = _ROLE_0;
+
+    IMagicKodiakVault public immutable vault;
+    address public immutable asset;
+    address public immutable token0;
+    address public immutable token1;
+
+    IKodiakV1RouterStaking public router;
+    address public exchangeRouter;
+
+    constructor(IMagicKodiakVault _vault, address _owner) {
+        vault = _vault;
+        _initializeOwner(_owner);
+
+        asset = IERC4626(address(vault)).asset();
+        asset.safeApprove(address(vault), type(uint256).max);
+        token0 = IKodiakVaultV1(address(vault)).token0();
+        token1 = IKodiakVaultV1(address(vault)).token1();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Operators
+    ////////////////////////////////////////////////////////////////////////////////
+
+    function run(SwapInfo[] memory swaps, uint256 amount0, uint256 amount1, uint256 minAmountOut) external onlyOwnerOrRoles(ROLE_OPERATOR) {
+        vault.harvest(address(this));
+
+        for (uint i = 0; i < swaps.length; i++) {
+            SwapInfo memory swap = swaps[i];
+
+            if (swap.swapData.length > 0) {
+                (bool success, ) = exchangeRouter.call(swap.swapData);
+                if (!success) {
+                    revert ErrSwapFailed();
+                }
+            }
+        }
+
+        uint balanceBefore = asset.balanceOf(address(this));
+        router.addLiquidity(IKodiakVaultV1(address(vault)), amount0, amount1, 0, 0, minAmountOut, address(this));
+
+        uint256 totalAmount = asset.balanceOf(address(this)) - balanceBefore;
+        (uint256 assetAmount, uint256 feeAmount) = _calculateFees(totalAmount);
+
+        if (feeAmount > 0) {
+            asset.safeTransfer(feeCollector, feeAmount);
+        }
+
+        vault.distributeRewards(assetAmount);
+        emit LogHarvest(totalAmount, assetAmount, feeAmount);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Admin
+    ////////////////////////////////////////////////////////////////////////////////
+
+    function setExchangeRouter(address _exchangeRouter) external onlyOwner {
+        IKodiakVaultStaking staking = vault.staking();
+
+        for (uint256 i = 0; i < staking.getAllRewardTokens().length; i++) {
+            address reward = staking.rewardTokens(i);
+
+            if (exchangeRouter != address(0)) {
+                reward.safeApprove(exchangeRouter, 0);
+            }
+            reward.safeApprove(_exchangeRouter, type(uint256).max);
+        }
+
+        emit LogExchangeRouterChanged(exchangeRouter, _exchangeRouter);
+        exchangeRouter = _exchangeRouter;
+    }
+
+    function setRouter(IKodiakV1RouterStaking _router) external onlyOwner {
+        if (address(router) != address(0)) {
+            token0.safeApprove(address(router), 0);
+            token1.safeApprove(address(router), 0);
+        }
+
+        token0.safeApprove(address(_router), type(uint256).max);
+        token1.safeApprove(address(_router), type(uint256).max);
+
+        emit LogRouterChanged(address(router), address(_router));
+        router = _router;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Internals
+    ////////////////////////////////////////////////////////////////////////////////
+
+    function _isFeeOperator(address account) internal view override returns (bool) {
+        return account == owner();
+    }
+}
