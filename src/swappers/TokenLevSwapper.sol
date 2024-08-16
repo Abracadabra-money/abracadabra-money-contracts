83174d3837f75ea0f83d9d6ee05fb102fa2a8e50
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2024-08-16 10:41:08 -0400

    updated token swappers for dynamic 'to'

--- src/swappers/TokenLevSwapper.sol
+++ src/swappers/TokenLevSwapper.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.0;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeTransferLib} from "@solady/utils/SafeTransferLib.sol";
 import {IBentoBoxLite} from "/interfaces/IBentoBoxV1.sol";
 import {ILevSwapperV2} from "/interfaces/ILevSwapperV2.sol";
@@ -12,28 +13,29 @@ contract TokenLevSwapper is ILevSwapperV2 {
     IBentoBoxLite public immutable box;
     address public immutable mim;
     address public immutable token;
-    address public immutable zeroXExchangeProxy;
 
-    constructor(IBentoBoxLite _box, address _token, address _mim, address _zeroXExchangeProxy) {
+    constructor(IBentoBoxLite _box, address _token, address _mim) {
         box = _box;
         token = _token;
         mim = _mim;
-        zeroXExchangeProxy = _zeroXExchangeProxy;
         _token.safeApprove(address(_box), type(uint256).max);
-        _mim.safeApprove(_zeroXExchangeProxy, type(uint256).max);
     }
 
-    /// @inheritdoc ILevSwapperV2
     function swap(
         address recipient,
         uint256 shareToMin,
         uint256 shareFrom,
-        bytes calldata swapData
+        bytes calldata data
     ) external override returns (uint256 extraShare, uint256 shareReturned) {
+        (address to, bytes memory swapData) = abi.decode(data, (address, bytes));
         box.withdraw(mim, address(this), address(this), 0, shareFrom);
 
+        if (IERC20(mim).allowance(address(this), to) != type(uint256).max) {
+            mim.safeApprove(to, type(uint256).max);
+        }
+
         // MIM -> token
-        (bool success, ) = zeroXExchangeProxy.call(swapData);
+        (bool success, ) = to.call(swapData);
         if (!success) {
             revert ErrSwapFailed();
         }
