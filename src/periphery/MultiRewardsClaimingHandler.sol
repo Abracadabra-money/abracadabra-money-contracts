1b014547dd32fbfc172e644c1f5c0d08694971bb
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2024-10-16 08:53:46 -0400

    ✨ BoundSPELL Review (#221)

--- src/periphery/MultiRewardsClaimingHandler.sol
+++ src/periphery/MultiRewardsClaimingHandler.sol
@@ -104,6 +104,10 @@ contract MultiRewardsClaimingHandler is IRewardHandler, OwnableOperators {
     ////////////////////////////////////////////////////////////////////
 
     function setRewardInfo(address _token, ILzOFTV2 _oft) external onlyOwner {
+        if (_oft != ILzOFTV2(address(0))) {
+            _token.safeApprove(address(_oft), type(uint256).max);
+        }
+
         tokenOfts[_token] = _oft;
         emit LogRewardInfoSet(_token, address(_oft));
     }
