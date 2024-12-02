8750605ea5d83353f856be0cd18f7818c97ab95b
0xMerlin <83640350+0xm3rlin@users.noreply.github.com>
2024-12-02 10:21:32 -0500

    Updated GMX deployment files (#232)
    Co-authored-by: Guardian <101481663+GuardianAudits@users.noreply.github.com>

--- src/interfaces/IGmxV2.sol
+++ src/interfaces/IGmxV2.sol
@@ -64,7 +64,6 @@ interface IGmxV2Deposit {
     // @param initialLongTokenAmount the amount of long tokens to deposit
     // @param initialShortTokenAmount the amount of short tokens to deposit
     // @param minMarketTokens the minimum acceptable number of liquidity tokens
-    // @param updatedAtBlock the block that the deposit was last updated at
     // sending funds back to the user in case the deposit gets cancelled
     // @param executionFee the execution fee for keepers
     // @param callbackGasLimit the gas limit for the callbackContract
@@ -72,7 +71,6 @@ interface IGmxV2Deposit {
         uint256 initialLongTokenAmount;
         uint256 initialShortTokenAmount;
         uint256 minMarketTokens;
-        uint256 updatedAtBlock;
         uint256 updatedAtTime;
         uint256 executionFee;
         uint256 callbackGasLimit;
@@ -121,11 +119,11 @@ interface IGmxV2Withdrawal {
         Flags flags;
     }
 
-    // @param account The account to withdraw for.
-    // @param receiver The address that will receive the withdrawn tokens.
-    // @param callbackContract The contract that will be called back.
-    // @param uiFeeReceiver The ui fee receiver.
-    // @param market The market on which the withdrawal will be executed.
+     // @param account The account to withdraw for.
+     // @param receiver The address that will receive the withdrawn tokens.
+     // @param callbackContract The contract that will be called back.
+     // @param uiFeeReceiver The ui fee receiver.
+     // @param market The market on which the withdrawal will be executed.
     struct Addresses {
         address account;
         address receiver;
@@ -136,17 +134,15 @@ interface IGmxV2Withdrawal {
         address[] shortTokenSwapPath;
     }
 
-    // @param marketTokenAmount The amount of market tokens that will be withdrawn.
-    // @param minLongTokenAmount The minimum amount of long tokens that must be withdrawn.
-    // @param minShortTokenAmount The minimum amount of short tokens that must be withdrawn.
-    // @param updatedAtBlock The block at which the withdrawal was last updated.
-    // @param executionFee The execution fee for the withdrawal.
-    // @param callbackGasLimit The gas limit for calling the callback contract.
+     // @param marketTokenAmount The amount of market tokens that will be withdrawn.
+     // @param minLongTokenAmount The minimum amount of long tokens that must be withdrawn.
+     // @param minShortTokenAmount The minimum amount of short tokens that must be withdrawn.
+     // @param executionFee The execution fee for the withdrawal.
+     // @param callbackGasLimit The gas limit for calling the callback contract.
     struct Numbers {
         uint256 marketTokenAmount;
         uint256 minLongTokenAmount;
         uint256 minShortTokenAmount;
-        uint256 updatedAtBlock;
         uint256 updatedAtTime;
         uint256 executionFee;
         uint256 callbackGasLimit;
