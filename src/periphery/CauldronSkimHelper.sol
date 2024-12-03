3f4ac515aa7a6351692f34218033791a1a0759f6
0xmDreamy <0xmDreamy@proton.me>
2024-12-03 20:03:58 +0100

    Add some comments to CauldronSkimHelper

--- src/periphery/CauldronSkimHelper.sol
+++ src/periphery/CauldronSkimHelper.sol
@@ -5,6 +5,9 @@ import {IBentoBoxLite} from "/interfaces/IBentoBoxV1.sol";
 import {ICauldronV2} from "/interfaces/ICauldronV2.sol";
 
 contract CauldronSkimHelper {
+    /// @notice Get the available skim amount for the caller cauldron.
+    /// Designed for use as a call action in `cook`. Typically followed
+    /// by an add collateral action that skims available amount of shares.
     function availableSkim() public view returns (uint256 share) {
         // Assume caller is a cauldron
         ICauldronV2 cauldron = ICauldronV2(msg.sender);
