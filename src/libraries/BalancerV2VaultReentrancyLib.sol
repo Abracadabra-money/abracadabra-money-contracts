58957fd4212f9b939b56a248361fc7a4c2c92d49
0xmDreamy <0xmDreamy@proton.me>
2025-02-22 00:56:56 +0100

    Add symbolic test for BalancerV2VaultReentrancyLib

--- src/libraries/BalancerV2VaultReentrancyLib.sol
+++ src/libraries/BalancerV2VaultReentrancyLib.sol
@@ -5,11 +5,14 @@ import {IBalancerV2Vault} from "/interfaces/IBalancerV2Vault.sol";
 
 library BalancerV2VaultReentrancyLib {
     function ensureNotInVaultContext(IBalancerV2Vault vault) internal view {
-        // Use low-level call allow it to be a view
+        // Use low-level static call
+        // Will always revert in reentrancy check modifier:
+        // - If reentrancy because of the reentrancy guard, revertData.length > 0
+        // - If not reentrancy because of modifying state which static calls do not permit, revertData.length == 0
         (, bytes memory revertData) = address(vault).staticcall(
             abi.encodeCall(vault.manageUserBalance, new IBalancerV2Vault.UserBalanceOp[](0))
         );
-        // Only check length as rentrancy guard always reverts with revertData.length > 0
+        // Only check length as it always reverts
         require(revertData.length == 0);
     }
 }
